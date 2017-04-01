40879c2c92e8a0ba07393956ddf80dd8c4aee50f
BorisPolonsky <qdogan@163.com>
2017-04-01 21:12:39 +0800

    Minor Update
    Log information for later use.

--- app/src/main/java/com/example/administrator/yamba/PrefActivity.java
+++ app/src/main/java/com/example/administrator/yamba/PrefActivity.java
@@ -7,6 +7,7 @@ package com.example.administrator.yamba;
 import android.app.Activity;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.util.Log;
 import android.widget.EditText;
 
 public class PrefActivity extends PreferenceActivity {
@@ -15,6 +16,7 @@ public class PrefActivity extends PreferenceActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG,"onCreate");
         addPreferencesFromResource(R.xml.pref);
     }
 }
