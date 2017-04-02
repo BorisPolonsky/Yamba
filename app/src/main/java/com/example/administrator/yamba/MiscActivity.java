193d68d9bfa4fb0e8986e7936bac75dd01ba6f31
BorisPolonsky <qdogan@163.com>
2017-04-02 14:15:20 +0800

    First Commit

---
+++ app/src/main/java/com/example/administrator/yamba/MiscActivity.java
@@ -0,0 +1,43 @@
+package com.example.administrator.yamba;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by Administrator on 2017/4/1.
+ */
+
+public class MiscActivity extends Activity
+{
+    abstract class ButtonClickListener implements View.OnClickListener
+    {
+    }
+    private Button buttonStartService;
+    private Button buttonStopService;
+    private static final String TAG=MiscActivity.class.getSimpleName();
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.i(TAG,"onCreate");
+        this.setContentView(R.layout.misc);
+        this.buttonStartService=(Button)findViewById(R.id.buttonStartService);
+        this.buttonStopService =(Button)findViewById(R.id.buttonStopService);
+        this.buttonStartService.setOnClickListener(new ButtonClickListener() {
+            @Override
+            public void onClick(View v) {
+                startService(new Intent(MiscActivity.this,UpdaterService.class));
+            }
+        });
+        this.buttonStopService.setOnClickListener(new ButtonClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopService(new Intent(MiscActivity.this,UpdaterService.class));
+            }
+        });
+    }
+}
