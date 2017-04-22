393ba91e9e70ee0d1bf94f0ab925f28776a236a7
Boris Polonsky <qdogan@163.com>
2017-04-22 22:29:43 +0800

    Minor Update
    This activity can read from the database now.

--- app/src/main/java/com/example/administrator/yamba/TimelineActivity.java
+++ app/src/main/java/com/example/administrator/yamba/TimelineActivity.java
@@ -1,11 +1,13 @@
 package com.example.administrator.yamba;
 
 import android.app.Activity;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.util.Log;
+import android.widget.TextView;
 
 /**
  * Created by Polonsky on 4/21/2017.
@@ -14,23 +16,36 @@ import android.util.Log;
 public class TimelineActivity extends Activity {
     private static final String TAG=TimelineActivity.class.getSimpleName();
     private StatusData statusData;
-
+    private SQLiteDatabase db;
+    private TextView timelineView;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.timeline_basic);
         this.statusData=new StatusData(this);
+        this.db=statusData.getDatabase();
+        this.timelineView=(TextView)findViewById(R.id.textTimeline);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        SQLiteDatabase db=statusData.getDatabase();
+        Cursor cursor=db.query("timeline",null,null,null,null,null,"created_at DESC");
+        String user,created_at,txt;
+        startManagingCursor(cursor);
+        while(cursor.moveToNext())//Will this miss the first row?
+        {
+            user=cursor.getString(cursor.getColumnIndex("user"));
+            created_at=cursor.getString(cursor.getColumnIndex("created_at"));
+            txt=cursor.getString(cursor.getColumnIndex("txt"));
+            timelineView.append(String.format("%s:\n%s\n",user,txt));
+        }
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        db.close();
         statusData.close();
         Log.i(TAG,"onDestroy");
     }
