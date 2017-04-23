290ec2278570dd935bfdc92dafb97c881066f4df
Boris Polonsky <qdogan@163.com>
2017-04-23 21:46:34 +0800

    Bug Fix
    In Yamba Application:
    Changed the field "username" to non-static field.
    Changed default settings of username.
    Added field password.

--- app/src/main/java/com/example/administrator/yamba/YambaApplication.java
+++ app/src/main/java/com/example/administrator/yamba/YambaApplication.java
@@ -18,14 +18,15 @@ public class YambaApplication extends Application implements
         SharedPreferences.OnSharedPreferenceChangeListener{
     SharedPreferences pref;
     private static final String TAG= YambaApplication.class.getSimpleName();
-    private static String username;
+    private String username=null;
+    private String password=null;
     @Override
     public void onCreate() {
         super.onCreate();
         this.pref= PreferenceManager.getDefaultSharedPreferences(this);
         this.pref.registerOnSharedPreferenceChangeListener(this);
-        this.pref.getString("username",null);
-        this.pref.getString("password",null);//Perhaps I would use these strings later...
+        this.username=this.pref.getString("username",null);
+        this.password=this.pref.getString("password",null);//Perhaps I would use these strings later...
     }
     @Override
     public void onTerminate() {
@@ -36,6 +37,9 @@ public class YambaApplication extends Application implements
     public synchronized void
     onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key)
     {
-        this.username=sharedPreferences.getString("username","");//For later use
+        if(key=="username")
+            this.username=sharedPreferences.getString("username",null);
+        if(key=="password")
+            this.password=sharedPreferences.getString("password",null);
     }
 }
