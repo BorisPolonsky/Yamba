5c1578ac911150d6051aaad70cab86ea6f307e2d
Boris Polonsky <qdogan@163.com>
2017-04-23 20:41:06 +0800

    Minor Update
    Used SimpleCursorAdapter and feed it with a ViewBinder instead of
    subclassing it as TimelineAdapter and rewrite its bindView method.

--- app/src/main/java/com/example/administrator/yamba/TimelineActivity.java
+++ app/src/main/java/com/example/administrator/yamba/TimelineActivity.java
@@ -6,7 +6,9 @@ import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
+import android.text.format.DateUtils;
 import android.util.Log;
+import android.view.View;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
@@ -20,7 +22,7 @@ public class TimelineActivity extends Activity {
 
     private SQLiteDatabase db;
     private ListView listTimeline;
-    private TimelineAdapter adapter;
+    private SimpleCursorAdapter adapter;
     private StatusData statusData;
     static final String[] FROM={"created_at","user","txt"};
     static final int[] TO={R.id.textCreatedAt,R.id.textUser,R.id.textText};
@@ -39,7 +41,21 @@ public class TimelineActivity extends Activity {
         Cursor cursor=db.query("timeline",null,null,null,null,null,"created_at DESC");
         String user,created_at,txt;
         startManagingCursor(cursor);
-        adapter=new TimelineAdapter(this,cursor);
+        adapter=new SimpleCursorAdapter(this,R.layout.row,cursor,FROM,TO);
+        adapter.setViewBinder(new SimpleCursorAdapter.ViewBinder(){
+            @Override
+            public boolean setViewValue(View view, Cursor cursor, int columnIndex) {
+                if (columnIndex==cursor.getColumnIndex(StatusData.C_CREATED_AT))
+                {
+                    TextView textCreatedAt=(TextView)view.findViewById(R.id.textCreatedAt);
+                    textCreatedAt.setText
+                            (DateUtils.getRelativeTimeSpanString(cursor.getLong(columnIndex)));
+                    return true;
+                }
+                else
+                    return false;
+            }
+        });
         listTimeline.setAdapter(adapter);
     }
 
