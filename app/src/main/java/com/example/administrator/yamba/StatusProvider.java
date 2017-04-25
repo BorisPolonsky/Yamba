d60087a1fcca843e46829b65660daebfd6efc024
Boris Polonsky <qdogan@163.com>
2017-04-25 23:07:04 +0800

    Bug Fix
    Made an instance of StatusData in onCreate().
    Otherwise the apps crash trying to invoke methods on null in query(),
    update() etc.

--- app/src/main/java/com/example/administrator/yamba/StatusProvider.java
+++ app/src/main/java/com/example/administrator/yamba/StatusProvider.java
@@ -8,6 +8,7 @@ import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.Log;
 
 /**
  * Created by Polonsky on 4/25/2017.
@@ -92,7 +93,9 @@ public class StatusProvider extends ContentProvider {
 
     @Override
     public boolean onCreate() {
-        return false;
+        this.statusData=new StatusData(this.getContext());
+        Log.i(TAG,"onCreate");
+        return true;//Return true if the provider is successfully loaded
     }
     private long getId(Uri uri){
         String lastPathSegment=uri.getLastPathSegment();
