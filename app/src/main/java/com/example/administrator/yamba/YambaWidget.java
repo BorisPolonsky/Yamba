d983d8f716d0bd91fb1a89b801a534428e51887c
Boris Polonsky <qdogan@163.com>
2017-04-25 22:19:26 +0800

    Minor Update
    A failed attempt to add a widget and a content provider.

---
+++ app/src/main/java/com/example/administrator/yamba/YambaWidget.java
@@ -0,0 +1,60 @@
+package com.example.administrator.yamba;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.text.format.DateUtils;
+import android.util.Log;
+import android.widget.RemoteViews;
+
+/**
+ * Created by Polonsky on 4/25/2017.
+ */
+
+public class YambaWidget extends AppWidgetProvider {
+    private static final String TAG=YambaWidget.class.getSimpleName();
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+        Cursor c=context.getContentResolver().query(StatusProvider.CONTENT_URI,null,null,null,null);//????
+        try{
+            if(c.moveToFirst()) {
+                CharSequence user = c.getString(c.getColumnIndex(StatusData.C_USER));
+                CharSequence createdAt= DateUtils.getRelativeTimeSpanString(
+                        c.getLong(c.getColumnIndex(StatusData.C_CREATED_AT)));
+                CharSequence text=c.getString(c.getColumnIndex(StatusData.C_TEXT));
+                for(int appWidgetId:appWidgetIds) {//Loop over widgets
+                    Log.i(TAG, "Updating widget " + appWidgetId);
+                    RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.yamba_widget);
+                    views.setTextViewText(R.id.textUser, user);
+                    views.setTextViewText(R.id.textCreatedAt,createdAt);
+                    views.setTextViewText(R.id.textText,text);
+                    views.setOnClickPendingIntent(R.id.yamba_icon,
+                            PendingIntent.getActivity(context,0,new Intent(context,TimelineActivity.class),0));
+                    appWidgetManager.updateAppWidget(appWidgetId,views);
+                }
+            }else{
+                Log.i(TAG,"No data to update");
+            }
+        }finally {
+            c.close();
+        }
+        Log.i(TAG,"onUpdate");
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+        if(intent.getAction().equals(UpdaterService.NEW_STATUS_INTENT)){//No action filters for this?
+            Log.i(TAG,"New status update detected");
+            AppWidgetManager appWidgetManager=AppWidgetManager.getInstance(context);
+            this.onUpdate(context,appWidgetManager,appWidgetManager.getAppWidgetIds(new ComponentName(context,YambaWidget.class)));//??????
+        }
+
+    }
+}
